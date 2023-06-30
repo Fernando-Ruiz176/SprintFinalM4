77de0ea581312b9958b7682bcf6016af81b5bc38
Fernando-Ruiz176 <ruizfe.sud@gmail.com>
2023-06-29 20:04:27 -0400

    Commit inicial

---
+++ app/src/main/java/cl/fernando/sprintfinalm4/MainActivity.java
@@ -0,0 +1,22 @@
+package cl.fernando.sprintfinalm4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import cl.fernando.sprintfinalm4.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+    }
+
+}
