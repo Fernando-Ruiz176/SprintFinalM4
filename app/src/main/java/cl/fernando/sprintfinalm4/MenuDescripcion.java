6fc6228db464d8afbb3048771f662afaa204218f
Fernando-Ruiz176 <ruizfe.sud@gmail.com>
2023-07-01 01:11:16 -0400

    Refactorizando codigo, se actualizan los botones y se mejora su funcionalidad.

--- app/src/main/java/cl/fernando/sprintfinalm4/MenuDescripcion.java
+++ app/src/main/java/cl/fernando/sprintfinalm4/MenuDescripcion.java
@@ -1,6 +1,9 @@
 package cl.fernando.sprintfinalm4;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -15,22 +18,50 @@ public class MenuDescripcion extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = ActivityMenuDescripcionBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-        //initListener();
+        initListener();
 
     }
 
-    //private void initListener() {
+    private void initListener() {
 
-        //binding.btnlinkedin.setOnClickListener(new View.OnClickListener() {
+        binding.btnlinkedin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentLinkedin = new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/tecnologiadeprogramacion"));
+                startActivity(intentLinkedin);
+            }
+        });
 
-            //@Override
-            //public void onClick(View v) {
-                //Intent intentLinkedin = new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/Fernando-Ruiz176"));
-                //startActivity(intentLinkedin);
-            //}
+        binding.btnWhatsapp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View w) {
+                String numeroTelefono = "+56977665544";
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:" + numeroTelefono));
+                startActivity(intent);
+            }
+        });
 
-        //});
-    //}
+        binding.btnContactame.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View vista) {
+
+                String emailText = binding.editTextMail.getText().toString();
+
+                Intent intent = new Intent(Intent.ACTION_SENDTO);
+                intent.setData(Uri.parse("mailto:"));
+                intent.putExtra(Intent.EXTRA_EMAIL, new String[]{"fernando.ruizfarias08@gmail.com"});
+                intent.putExtra(Intent.EXTRA_SUBJECT, "Nuevo Mensaje");
+                intent.putExtra(Intent.EXTRA_TEXT, emailText);
+
+                if (intent.resolveActivity(getPackageManager()) != null) {
+                    startActivity(intent);
+                }
+            }
+        });
+
+
+    }
 
 
 }
